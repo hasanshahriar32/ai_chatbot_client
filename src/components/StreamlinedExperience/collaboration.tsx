d2839f9a672acb4324b49c8123933d4bd08981c9
Mr. Hasan <hasanshahriar32@gmail.com>
2023-07-23 00:23:28 +0600

    updated text style

--- src/components/StreamlinedExperience/collaboration.tsx
+++ src/components/StreamlinedExperience/collaboration.tsx
@@ -1,12 +1,18 @@
 import { useTransform, useScroll, motion } from "framer-motion";
 import { useRef } from "react";
 import React from "react";
+import { isSafari } from "react-device-detect";
+import { useInView } from "react-intersection-observer";
 export const Collaboration = () => {
   const targetRef = useRef<HTMLDivElement | null>(null);
   const extendedRef = useRef<HTMLDivElement | null>(null);
   const { scrollYProgress } = useScroll({
     target: targetRef,
     offset: ["start end", "end end"],
+  });
+  const [ref, inView] = useInView({
+    triggerOnce: false, // Only trigger once when the component comes into view
+    threshold: 0.1, // Adjust the threshold value as per your needs
   });
   const { scrollYProgress: scrollYProgressIncludingOverlap } = useScroll({
     target: extendedRef,
@@ -15,6 +21,19 @@ export const Collaboration = () => {
   const position = useTransform(scrollYProgress, (pos) =>
     pos >= 0.3 && pos <= 1 ? "fixed" : "relative"
   );
+  const container = {
+    hidden: { opacity: 1, scale: 0 },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      rotate: 0,
+      transition: {
+        delayChildren: 0,
+        duration: 1.5,
+        staggerChildren: 0.2,
+      },
+    },
+  };
   const positions = useTransform(scrollYProgress, (pos) =>
     pos <= 9 ? "relative" : "hidden"
   );
@@ -40,7 +59,7 @@ export const Collaboration = () => {
     [0, 0.23, 0.25],
     [0, 0, 1]
   );
-
+  const text = "Three in One";
   const avatarGroupX = useTransform(
     scrollYProgress,
     [0, 0.23, 0.25, 0.4, 0.45, 0.6, 0.65],
@@ -105,12 +124,31 @@ export const Collaboration = () => {
             </motion.div>
           </div>
           <motion.h2
+            ref={ref}
             className="absolute font-sans scroll-smooth flex items-center flex-col text-6xl lg:text-8xl text-success top-2 right-1 h-screen justify-center"
             style={{ opacity: avatarThreeScale }}
           >
-            <motion.span>Three</motion.span>
+            <motion.div
+              initial={!isSafari ? { rotate: 0 } : "scale: 0"}
+              className="text container"
+            >
+              {text.split("").map((letter, index) => (
+                <motion.span
+                  variants={container}
+                  key={index}
+                  initial={{ opacity: 0 }}
+                  // animate={{}}
+                  animate={inView ? { opacity: 1 } : "rotate: 360, scale: 1"}
+                  className="text container"
+                  transition={{ duration: 1, delay: index * 0.1 }}
+                >
+                  {letter}
+                </motion.span>
+              ))}
+            </motion.div>
+            {/* <motion.span>Three</motion.span>
             <motion.span>in</motion.span>
-            <motion.span>One</motion.span>
+            <motion.span>One</motion.span> */}
           </motion.h2>
         </motion.div>
       </div>
