f1f641322c79507683640f6fbc941720991f5cc2
Mr. Hasan <hasanshahriar32@gmail.com>
2023-07-27 12:17:09 +0600

    update

--- src/components/Hero/Developer.jsx
+++ src/components/Hero/Developer.jsx
@@ -13,9 +13,24 @@ const Developer = () => {
         className="z-30 btn btn-ghost btn-link cursor-pointer"
       >
         <HeroTitle className=" text-center hover:shadow-lg shadow translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:200ms]">
-          <span className="hidden visible">Visit my</span>{" "}
+          {/* <span className="hidden visible">Visit my</span>{" "} */}
           <span>
             Portfolio <RiExternalLinkLine />
+            {/* add svg icon of redirect */}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6 inline-block"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M17 8l4 4m0 0l-4 4m4-4H3"
+              />
+            </svg>
           </span>
         </HeroTitle>
       </button>
