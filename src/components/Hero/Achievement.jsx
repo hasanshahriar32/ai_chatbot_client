6f38401c1e60d015042511e434bb6f28c72b450c
Mr. Hasan <hasanshahriar32@gmail.com>
2023-07-29 09:43:45 +0600

    updated

--- src/components/Hero/Achievement.jsx
+++ src/components/Hero/Achievement.jsx
@@ -10,7 +10,7 @@ const Achievement = () => {
               className="w-full"
             />
             <div className="absolute pt-15 flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-              <a href="#slide4" className="btn btn-circle">
+              <a href="#slide2" className="btn btn-circle">
                 ❮
               </a>
               <a href="#slide2" className="btn btn-circle">
@@ -27,7 +27,7 @@ const Achievement = () => {
               <a href="#slide1" className="btn btn-circle">
                 ❮
               </a>
-              <a href="#slide3" className="btn btn-circle">
+              <a href="#slide1" className="btn btn-circle">
                 ❯
               </a>
             </div>
