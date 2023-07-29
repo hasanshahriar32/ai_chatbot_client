f9357338596d7917c982e4476d4e59047d1a89be
Mr. Hasan <hasanshahriar32@gmail.com>
2023-07-29 08:39:38 +0600

    updated cert

--- src/components/Hero/Achievement.jsx
+++ src/components/Hero/Achievement.jsx
@@ -2,9 +2,37 @@ const Achievement = () => {
   return (
     <div className=" flex justify-center flex-col-reverse items-center">
       <div className="flex flex-col justify-center gap-2 m-[5%]">
-        <h1 className="text-5xl font-bold text-center mb-10">Certificates</h1>
-        <img src="https://i.ibb.co/yhYgXdp/Neuronex.jpg" alt="" />
-        <img src="https://i.ibb.co/nbs4WhP/Shahriar-Hasan.jpg" alt="" />
+        <h1 className="text-5xl font-bold text-center mt-10">Certificates</h1>
+        <div className="carousel w-full">
+          <div id="slide1" className="carousel-item relative w-full  pt-15">
+            <img
+              src="https://i.ibb.co/yhYgXdp/Neuronex.jpg"
+              className="w-full"
+            />
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+              <a href="#slide4" className="btn btn-circle">
+                ❮
+              </a>
+              <a href="#slide2" className="btn btn-circle">
+                ❯
+              </a>
+            </div>
+          </div>
+          <div id="slide2" className="carousel-item relative w-full pt-15">
+            <img
+              src="https://i.ibb.co/nbs4WhP/Shahriar-Hasan.jpg"
+              className="w-full"
+            />
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+              <a href="#slide1" className="btn btn-circle">
+                ❮
+              </a>
+              <a href="#slide3" className="btn btn-circle">
+                ❯
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
       <div className="my-10 z-10">
         <h1 className="text-5xl font-bold text-center md:text-left mb-10">
