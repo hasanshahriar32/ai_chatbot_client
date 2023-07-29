ce2f2c29feb27b97dc0a743e710e875a0904be93
Mr. Hasan <hasanshahriar32@gmail.com>
2023-07-29 09:35:03 +0600

    updated

--- src/components/Hero/Achievement.jsx
+++ src/components/Hero/Achievement.jsx
@@ -9,7 +9,7 @@ const Achievement = () => {
               src="https://i.ibb.co/yhYgXdp/Neuronex.jpg"
               className="w-full"
             />
-            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <div className="absolute pt-15 flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
               <a href="#slide4" className="btn btn-circle">
                 ❮
               </a>
@@ -23,7 +23,7 @@ const Achievement = () => {
               src="https://i.ibb.co/nbs4WhP/Shahriar-Hasan.jpg"
               className="w-full"
             />
-            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <div className="absolute flex pt-15 justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
               <a href="#slide1" className="btn btn-circle">
                 ❮
               </a>
